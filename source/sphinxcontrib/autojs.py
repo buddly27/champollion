9ee7e4548ed8c87c897387f4fe460ddcad969917
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2017-05-28 23:47:07 -0700

    Setup directives and connections

---
+++ source/sphinxcontrib/autojs.py
@@ -0,0 +1,33 @@
+# :coding: utf-8
+
+import os
+
+from directive import AutoDataDirective
+from viewcode import (
+    add_source_code_links,
+    create_code_pages,
+    create_missing_code_link
+)
+import parser
+
+
+def parse_js_source(app):
+    """Parse the javascript source path."""
+    path = os.path.abspath(app.config.js_source)
+    app.env.js_environment = parser.get_environment(path)
+
+
+def setup(app):
+    """Register the javascript autodoc directives."""
+    app.connect("builder-inited", parse_js_source)
+
+    app.add_directive_to_domain("js", "autodata", AutoDataDirective)
+    # app.add_directive_to_domain("js", "automodule", AutoModuleDirective)
+    # app.add_directive_to_domain("js", "autoclass", AutoClassDirective)
+    # app.add_directive_to_domain("js", "autofunction", AutoFunctionDirective)
+
+    app.add_config_value("js_source", None, "env")
+
+    app.connect("doctree-read", add_source_code_links)
+    app.connect("html-collect-pages", create_code_pages)
+    app.connect("missing-reference", create_missing_code_link)
