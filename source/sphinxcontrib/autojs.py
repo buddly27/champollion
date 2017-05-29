d41ad0545975012d52bddcd45bf50907a270f29d
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2017-05-28 23:51:23 -0700

    Group callback connections together

--- source/sphinxcontrib/autojs.py
+++ source/sphinxcontrib/autojs.py
@@ -19,15 +19,14 @@ def parse_js_source(app):
 
 def setup(app):
     """Register the javascript autodoc directives."""
+    app.add_config_value("js_source", None, "env")
+
     app.connect("builder-inited", parse_js_source)
+    app.connect("doctree-read", add_source_code_links)
+    app.connect("html-collect-pages", create_code_pages)
+    app.connect("missing-reference", create_missing_code_link)
 
     app.add_directive_to_domain("js", "autodata", AutoDataDirective)
     # app.add_directive_to_domain("js", "automodule", AutoModuleDirective)
     # app.add_directive_to_domain("js", "autoclass", AutoClassDirective)
     # app.add_directive_to_domain("js", "autofunction", AutoFunctionDirective)
-
-    app.add_config_value("js_source", None, "env")
-
-    app.connect("doctree-read", add_source_code_links)
-    app.connect("html-collect-pages", create_code_pages)
-    app.connect("missing-reference", create_missing_code_link)
