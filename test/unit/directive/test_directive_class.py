2f620c098b4b1ad023274b1e3848c1dfc50c2be0
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2017-06-03 22:31:21 -0700

    Add unit tests for class directive 'members' options

--- test/unit/directive/test_directive_class.py
+++ test/unit/directive/test_directive_class.py
@@ -62,17 +62,18 @@ def content():
         "    }\n"
         "\n"
         "    /**\n"
-        "     * awesomeMethod1.\n"
+        "     * awesomeMethod.\n"
         "     */\n"
-        "    awesomeMethod1 = () => {\n"
-        "        console.log('Method 1 has been called');\n"
+        "    awesomeMethod = () => {\n"
+        "        console.log('Method has been called');\n"
         "    };\n"
         "\n"
-        "    /**\n"
-        "     * awesomeMethod2.\n"
-        "     */\n"
-        "    awesomeMethod2(arg1, arg2) {\n"
-        "        console.log('Method 2 has been called');\n"
+        "    undocumentedMethod(arg1, arg2) {\n"
+        "        console.log('An un-documented method has been called');\n"
+        "    }\n"
+        "\n"
+        "    _privateMethod(arg1) {\n"
+        "        console.log('An private method has been called');\n"
         "    }\n"
         "\n"
         "    /**\n"
@@ -128,11 +129,51 @@ def test_directive_autoclass(doc_folder, content):
             "\n"
             "   Base Class\n"
             "\n"
-            "   constructor()\n"
-            "\n"
             "class example.CustomWelcome()\n"
             "\n"
-            "   greeting()\n"
+            "class example.AwesomeClass(name)\n"
+            "\n"
+            "   \"import AwesomeClass from \"example\"\"\n"
+            "\n"
+            "   Inherited class\n"
+        )
+
+
+def test_directive_autoclass_with_members(doc_folder, content):
+    """Generate documentation from classes.
+    """
+    js_source = os.path.join(doc_folder, "example")
+    with open(os.path.join(js_source, "index.js"), "w") as f:
+        f.write(content)
+
+    index_file = os.path.join(doc_folder, "index.rst")
+    with open(index_file, "w") as f:
+        f.write(
+            ".. js:autoclass:: example.MotherClass\n"
+            "    :members:\n"
+            "\n"
+            ".. js:autoclass:: example.CustomWelcome\n"
+            "    :members:\n"
+            "\n"
+            ".. js:autoclass:: example.AwesomeClass\n"
+            "    :members:\n"
+        )
+
+    with cd(doc_folder):
+        sphinx_main(["dummy", "-b", "text", "-E", ".", "_build"])
+
+    with open(os.path.join(doc_folder, "_build", "index.txt"), "r") as f:
+        if sys.version_info < (3, 0):
+            content = f.read().decode("ascii", "ignore")
+        else:
+            content = f.read().encode("ascii", "ignore").decode("utf8")
+
+        assert content == (
+            "class example.MotherClass()\n"
+            "\n"
+            "   Base Class\n"
+            "\n"
+            "class example.CustomWelcome()\n"
             "\n"
             "class example.AwesomeClass(name)\n"
             "\n"
@@ -156,13 +197,9 @@ def test_directive_autoclass(doc_folder, content):
             "\n"
             "      Warning: Keep the name awesome\n"
             "\n"
-            "   awesomeMethod1()\n"
+            "   awesomeMethod()\n"
             "\n"
-            "      awesomeMethod1.\n"
-            "\n"
-            "   awesomeMethod2(arg1, arg2)\n"
-            "\n"
-            "      awesomeMethod2.\n"
+            "      awesomeMethod.\n"
             "\n"
             "   static staticMethod()\n"
             "\n"
@@ -178,3 +215,166 @@ def test_directive_autoclass(doc_folder, content):
             "\n"
             "      another attribute.\n"
         )
+
+
+def test_directive_autoclass_with_default_members(doc_folder, content):
+    """Generate documentation from classes.
+    """
+    conf_file = os.path.join(doc_folder, "conf.py")
+    with open(conf_file, "a") as f:
+        f.write("\njs_class_options = ['members']")
+
+    js_source = os.path.join(doc_folder, "example")
+    with open(os.path.join(js_source, "index.js"), "w") as f:
+        f.write(content)
+
+    index_file = os.path.join(doc_folder, "index.rst")
+    with open(index_file, "w") as f:
+        f.write(
+            ".. js:autoclass:: example.MotherClass\n"
+            "\n"
+            ".. js:autoclass:: example.CustomWelcome\n"
+            "\n"
+            ".. js:autoclass:: example.AwesomeClass\n"
+        )
+
+    with cd(doc_folder):
+        sphinx_main(["dummy", "-b", "text", "-E", ".", "_build"])
+
+    with open(os.path.join(doc_folder, "_build", "index.txt"), "r") as f:
+        if sys.version_info < (3, 0):
+            content = f.read().decode("ascii", "ignore")
+        else:
+            content = f.read().encode("ascii", "ignore").decode("utf8")
+
+        assert content == (
+            "class example.MotherClass()\n"
+            "\n"
+            "   Base Class\n"
+            "\n"
+            "class example.CustomWelcome()\n"
+            "\n"
+            "class example.AwesomeClass(name)\n"
+            "\n"
+            "   \"import AwesomeClass from \"example\"\"\n"
+            "\n"
+            "   Inherited class\n"
+            "\n"
+            "   constructor(name)\n"
+            "\n"
+            "      Constructor.\n"
+            "\n"
+            "   get name()\n"
+            "\n"
+            "      Get name.\n"
+            "\n"
+            "      Warning: The name is awesome\n"
+            "\n"
+            "   set name(value)\n"
+            "\n"
+            "      Set name.\n"
+            "\n"
+            "      Warning: Keep the name awesome\n"
+            "\n"
+            "   awesomeMethod()\n"
+            "\n"
+            "      awesomeMethod.\n"
+            "\n"
+            "   static staticMethod()\n"
+            "\n"
+            "      staticMethod.\n"
+            "\n"
+            "   static attribute = 42\n"
+            "\n"
+            "      attribute.\n"
+            "\n"
+            "   classicAttribute = {\n"
+            "           test: a test,\n"
+            "       }\n"
+            "\n"
+            "      another attribute.\n"
+        )
+
+
+
+# def test_directive_autoclass(doc_folder, content):
+#     """Generate documentation from classes.
+#     """
+#     js_source = os.path.join(doc_folder, "example")
+#     with open(os.path.join(js_source, "index.js"), "w") as f:
+#         f.write(content)
+#
+#     index_file = os.path.join(doc_folder, "index.rst")
+#     with open(index_file, "w") as f:
+#         f.write(
+#             ".. js:autoclass:: example.MotherClass\n"
+#             "\n"
+#             ".. js:autoclass:: example.CustomWelcome\n"
+#             "\n"
+#             ".. js:autoclass:: example.AwesomeClass\n"
+#         )
+#
+#     with cd(doc_folder):
+#         sphinx_main(["dummy", "-b", "text", "-E", ".", "_build"])
+#
+#     with open(os.path.join(doc_folder, "_build", "index.txt"), "r") as f:
+#         if sys.version_info < (3, 0):
+#             content = f.read().decode("ascii", "ignore")
+#         else:
+#             content = f.read().encode("ascii", "ignore").decode("utf8")
+#
+#         assert content == (
+#             "class example.MotherClass()\n"
+#             "\n"
+#             "   Base Class\n"
+#             "\n"
+#             "   constructor()\n"
+#             "\n"
+#             "class example.CustomWelcome()\n"
+#             "\n"
+#             "   greeting()\n"
+#             "\n"
+#             "class example.AwesomeClass(name)\n"
+#             "\n"
+#             "   \"import AwesomeClass from \"example\"\"\n"
+#             "\n"
+#             "   Inherited class\n"
+#             "\n"
+#             "   constructor(name)\n"
+#             "\n"
+#             "      Constructor.\n"
+#             "\n"
+#             "   get name()\n"
+#             "\n"
+#             "      Get name.\n"
+#             "\n"
+#             "      Warning: The name is awesome\n"
+#             "\n"
+#             "   set name(value)\n"
+#             "\n"
+#             "      Set name.\n"
+#             "\n"
+#             "      Warning: Keep the name awesome\n"
+#             "\n"
+#             "   awesomeMethod1()\n"
+#             "\n"
+#             "      awesomeMethod1.\n"
+#             "\n"
+#             "   awesomeMethod2(arg1, arg2)\n"
+#             "\n"
+#             "      awesomeMethod2.\n"
+#             "\n"
+#             "   static staticMethod()\n"
+#             "\n"
+#             "      staticMethod.\n"
+#             "\n"
+#             "   static attribute = 42\n"
+#             "\n"
+#             "      attribute.\n"
+#             "\n"
+#             "   classicAttribute = {\n"
+#             "           test: a test,\n"
+#             "       }\n"
+#             "\n"
+#             "      another attribute.\n"
+#         )
