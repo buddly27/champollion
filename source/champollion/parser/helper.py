525a9ee2c5c7f774b3343d06dd89066d23bf93a5
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2017-06-10 12:08:33 -0700

    Remove debug prints

--- source/champollion/parser/helper.py
+++ source/champollion/parser/helper.py
@@ -343,18 +343,3 @@ def get_binding_environment(expression):
         environments.append(_module)
 
     return environments
-
-
-# export { name1, name2, …, nameN };
-# export { variable1 as name1, variable2 as name2, …, nameN };
-# export let name1, name2, …, nameN; // also var
-# export let name1 = …, name2 = …, …, nameN; // also var, const
-#
-# export default expression;
-# export default function (…) { … } // also class, function*
-# export default function name1(…) { … } // also class, function*
-# export { name1 as default, … };
-#
-# export * from …;
-# export { name1, name2, …, nameN } from …;
-# export { import1 as name1, import2 as name2, …, nameN } from …;
