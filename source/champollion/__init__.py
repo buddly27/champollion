ede08eb89edb008dba1fac7971c90743826d0326
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2017-06-11 14:23:11 -0700

    Rename 'parse_js_source' to 'fetch_javascript_environment' for clarity

--- source/champollion/__init__.py
+++ source/champollion/__init__.py
@@ -19,23 +19,12 @@ from .viewcode import (
 from .parser import fetch_environment
 
 
-def parse_js_source(app):
-    """Parse the javascript source path and store it in *app* environment.
-
-    This function is called with the ``builder-inited`` Sphinx event, emitted
-    when the builder object is created.
-
-    """
-    path = os.path.abspath(app.config.js_source)
-    app.env.js_environment = fetch_environment(path)
-
-
 def setup(app):
     """Register callbacks and directives."""
     app.add_config_value("js_source", None, "env")
     app.add_config_value("js_class_options", [], "env")
 
-    app.connect("builder-inited", parse_js_source)
+    app.connect("builder-inited", fetch_javascript_environment)
     app.connect("doctree-read", add_source_code_links)
     app.connect("html-collect-pages", create_code_pages)
     app.connect("missing-reference", create_missing_code_link)
@@ -50,3 +39,16 @@ def setup(app):
     return {
         "version": __version__
     }
+
+
+def fetch_javascript_environment(app):
+    """Parse the :term:`Javascript` source path and store it in *app*
+    environment.
+
+    This function is called with the ``builder-inited`` Sphinx event, emitted
+    when the builder object is created.
+
+    """
+    path = os.path.abspath(app.config.js_source)
+    app.env.js_environment = fetch_environment(path)
+
