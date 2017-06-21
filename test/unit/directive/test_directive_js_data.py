73f7b19749b11c95ab60ca953658738527c19515
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2017-06-20 19:26:02 -0700

    Add data directive unit tests for options

--- test/unit/directive/test_directive_js_data.py
+++ test/unit/directive/test_directive_js_data.py
@@ -85,3 +85,163 @@ def test_directive_autodata(doc_folder, content):
             "\n"
             "   \"import {VARIABLE_STRING} from \"example\"\"\n"
         )
+
+
+def test_directive_autodata_with_alias(doc_folder, content):
+    """Generate documentation from global data variables with alias.
+    """
+    js_source = os.path.join(doc_folder, "example")
+    with open(os.path.join(js_source, "index.js"), "w") as f:
+        f.write(content)
+
+    index_file = os.path.join(doc_folder, "index.rst")
+    with open(index_file, "w") as f:
+        f.write(
+            ".. js:autodata:: example.VARIABLE_INT\n"
+            "    :alias: ALIASED_VARIABLE_INT\n"
+            "\n"
+            ".. js:autodata:: example.VARIABLE_OBJECT\n"
+            "    :alias: ALIASED_VARIABLE_OBJECT\n"
+            "\n"
+            ".. js:autodata:: example.VARIABLE_STRING\n"
+            "    :alias: ALIASED_VARIABLE_STRING\n"
+        )
+
+    with cd(doc_folder):
+        sphinx_main(["dummy", "-b", "text", "-E", ".", "_build"])
+
+    with open(os.path.join(doc_folder, "_build", "index.txt"), "r") as f:
+        if sys.version_info < (3, 0):
+            content = f.read().decode("ascii", "ignore")
+        else:
+            content = f.read().encode("ascii", "ignore").decode("utf8")
+
+        assert content == (
+            "const example.ALIASED_VARIABLE_INT = 42\n"
+            "\n"
+            "   \"import ALIASED_VARIABLE_INT from \"example\"\"\n"
+            "\n"
+            "   A variable\n"
+            "\n"
+            "   Note: A note.\n"
+            "\n"
+            "var example.ALIASED_VARIABLE_OBJECT = { "
+            "key1: value1, key2: value2, key3: value3, }\n"
+            "\n"
+            "   Another variable\n"
+            "\n"
+            "   A citation:\n"
+            "\n"
+            "      A citation\n"
+            "\n"
+            "let example.ALIASED_VARIABLE_STRING = rosebud\n"
+            "\n"
+            "   \"import {ALIASED_VARIABLE_STRING} from \"example\"\"\n"
+        )
+
+
+def test_directive_autodata_with_module_alias(doc_folder, content):
+    """Generate documentation from global data variables with module alias.
+    """
+    js_source = os.path.join(doc_folder, "example")
+    with open(os.path.join(js_source, "index.js"), "w") as f:
+        f.write(content)
+
+    index_file = os.path.join(doc_folder, "index.rst")
+    with open(index_file, "w") as f:
+        f.write(
+            ".. js:autodata:: example.VARIABLE_INT\n"
+            "    :module-alias: alias_module\n"
+            "\n"
+            ".. js:autodata:: example.VARIABLE_OBJECT\n"
+            "    :module-alias: alias_module\n"
+            "\n"
+            ".. js:autodata:: example.VARIABLE_STRING\n"
+            "    :module-alias: alias_module\n"
+        )
+
+    with cd(doc_folder):
+        sphinx_main(["dummy", "-b", "text", "-E", ".", "_build"])
+
+    with open(os.path.join(doc_folder, "_build", "index.txt"), "r") as f:
+        if sys.version_info < (3, 0):
+            content = f.read().decode("ascii", "ignore")
+        else:
+            content = f.read().encode("ascii", "ignore").decode("utf8")
+
+        assert content == (
+            "const alias_module.VARIABLE_INT = 42\n"
+            "\n"
+            "   \"import VARIABLE_INT from \"alias_module\"\"\n"
+            "\n"
+            "   A variable\n"
+            "\n"
+            "   Note: A note.\n"
+            "\n"
+            "var alias_module.VARIABLE_OBJECT = { "
+            "key1: value1, key2: value2, key3: value3, }\n"
+            "\n"
+            "   Another variable\n"
+            "\n"
+            "   A citation:\n"
+            "\n"
+            "      A citation\n"
+            "\n"
+            "let alias_module.VARIABLE_STRING = rosebud\n"
+            "\n"
+            "   \"import {VARIABLE_STRING} from \"alias_module\"\"\n"
+        )
+
+
+def test_directive_autodata_with_partial_import_forced(doc_folder, content):
+    """Generate documentation from global data variables with partial import
+    forced.
+    """
+    js_source = os.path.join(doc_folder, "example")
+    with open(os.path.join(js_source, "index.js"), "w") as f:
+        f.write(content)
+
+    index_file = os.path.join(doc_folder, "index.rst")
+    with open(index_file, "w") as f:
+        f.write(
+            ".. js:autodata:: example.VARIABLE_INT\n"
+            "    :force-partial-import:\n"
+            "\n"
+            ".. js:autodata:: example.VARIABLE_OBJECT\n"
+            "    :force-partial-import:\n"
+            "\n"
+            ".. js:autodata:: example.VARIABLE_STRING\n"
+            "    :force-partial-import:\n"
+        )
+
+    with cd(doc_folder):
+        sphinx_main(["dummy", "-b", "text", "-E", ".", "_build"])
+
+    with open(os.path.join(doc_folder, "_build", "index.txt"), "r") as f:
+        if sys.version_info < (3, 0):
+            content = f.read().decode("ascii", "ignore")
+        else:
+            content = f.read().encode("ascii", "ignore").decode("utf8")
+
+        assert content == (
+            "const example.VARIABLE_INT = 42\n"
+            "\n"
+            "   \"import {VARIABLE_INT} from \"example\"\"\n"
+            "\n"
+            "   A variable\n"
+            "\n"
+            "   Note: A note.\n"
+            "\n"
+            "var example.VARIABLE_OBJECT = { "
+            "key1: value1, key2: value2, key3: value3, }\n"
+            "\n"
+            "   Another variable\n"
+            "\n"
+            "   A citation:\n"
+            "\n"
+            "      A citation\n"
+            "\n"
+            "let example.VARIABLE_STRING = rosebud\n"
+            "\n"
+            "   \"import {VARIABLE_STRING} from \"example\"\"\n"
+        )
