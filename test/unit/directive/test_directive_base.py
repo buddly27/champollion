871a335e72bb3083734e466a71caa7597912d961
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2017-06-20 20:02:14 -0700

    Remove unnecessary fixture

--- test/unit/directive/test_directive_base.py
+++ test/unit/directive/test_directive_base.py
@@ -7,26 +7,21 @@ from sphinx.cmdline import main as sphinx_main
 from sphinx.util.osutil import cd
 
 
-@pytest.fixture()
-def content():
-    return (
-        "/**\n"
-        " * A variable\n"
-        " *\n"
-        " * .. note::\n"
-        " *\n"
-        " *     A note.\n"
-        " */\n"
-        "const VARIABLE = 42;\n"
-    )
-
-
-def test_directive_error(doc_folder, content):
+def test_directive_error(doc_folder):
     """Do not generate doc for non existing data.
     """
     js_source = os.path.join(doc_folder, "example")
     with open(os.path.join(js_source, "index.js"), "w") as f:
-        f.write(content)
+        f.write(
+            "/**\n"
+            " * A variable\n"
+            " *\n"
+            " * .. note::\n"
+            " *\n"
+            " *     A note.\n"
+            " */\n"
+            "const VARIABLE = 42;\n"
+        )
 
     index_file = os.path.join(doc_folder, "index.rst")
     with open(index_file, "w") as f:
