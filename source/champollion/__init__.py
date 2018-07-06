5da8e8ac5eac13dbd49d7bb7047bb47728da2821
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2018-07-06 16:39:24 -0700

    Add 'js_sources' configuration value to provide several code paths

--- source/champollion/__init__.py
+++ source/champollion/__init__.py
@@ -18,6 +18,7 @@ from .parser import fetch_environment
 def setup(app):
     """Register callbacks and directives."""
     app.add_config_value("js_source", None, True)
+    app.add_config_value("js_sources", [], True)
     app.add_config_value("js_environment", None, True)
     app.add_config_value("js_class_options", [], True)
     app.add_config_value("js_module_options", [], True)
@@ -43,7 +44,8 @@ def fetch_javascript_environment(app):
     """Fetch the :term:`Javascript` environment from the *app* configuration.
 
     If the **js_environment** configuration is not provided, attempt to parse
-    the path provided via the **js_source** configuration value.
+    the path provided via the **js_source** or **js_sources** configuration
+    value.
 
     This function is called with the ``builder-inited`` Sphinx event, emitted
     when the builder object is created.
@@ -53,6 +55,26 @@ def fetch_javascript_environment(app):
         :ref:`configuration`
 
     """
-    if app.config.js_environment is None:
+    if app.config.js_environment is not None:
+        return
+
+    if app.config.js_source is not None:
         path = os.path.abspath(app.config.js_source)
         app.config.js_environment = fetch_environment(path)
+
+    elif len(app.config.js_sources) > 0:
+        app.config.js_environment = {}
+
+        for path in app.config.js_sources:
+            path = os.path.abspath(path)
+
+            _environment = fetch_environment(path)
+            for key in _environment.keys():
+                app.config.js_environment.setdefault(key, {})
+                app.config.js_environment[key].update(_environment[key])
+
+    else:
+        raise RuntimeError(
+            "Either the 'js_source' or the 'js_sources' configuration value "
+            "must be provided."
+        )
